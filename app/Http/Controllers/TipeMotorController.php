caf8bdf084ce1a3802c34fd87bf759c496d67714
Agiftsany Azhar <agiftsanyazhar@gmail.com>
2023-11-09 11:31:10 +0700

    data master done

---
+++ app/Http/Controllers/TipeMotorController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TipeMotor;
+use App\Http\Requests\StoreTipeMotorRequest;
+use App\Http\Requests\UpdateTipeMotorRequest;
+
+class TipeMotorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTipeMotorRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TipeMotor $tipeMotor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TipeMotor $tipeMotor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTipeMotorRequest $request, TipeMotor $tipeMotor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TipeMotor $tipeMotor)
+    {
+        //
+    }
+}
